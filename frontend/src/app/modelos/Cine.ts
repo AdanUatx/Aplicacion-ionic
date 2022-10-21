be2b8f5c435912f3950bf913b84392615e2b7aaf
AdanUATx <20191445@uatx.mx>
2022-10-21 11:19:17 -0500

    FILES SERVICIOS

---
+++ frontend/src/app/modelos/Cine.ts
@@ -0,0 +1,17 @@
+
+export class Cine{
+ cineID: number;
+ cine: string;
+ fechaAlta: string;
+ fechaModificacion: string;
+ activo: boolean;
+
+
+  constructor() {
+    this.cineID = null;
+    this.cine = null;
+    this.fechaAlta = null;
+    this.fechaModificacion = null;
+    this.activo = null;
+  }
+}
